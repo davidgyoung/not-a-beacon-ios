d126824a708275f66959e75ef2e6f7511fa6ca87
David G. Young <davidgyoung@gmail.com>
2024-11-16 19:33:13 -0500

    iOS Test code

---
+++ NotABeacon/NotABeaconScanner.swift
@@ -0,0 +1,41 @@
+//
+//  NotABeaconScanner.swift
+//  NotABeacon
+//
+//  Created by David G. Young on 11/16/24.
+//
+
+import CoreBluetooth
+
+class NotABeaconScanner: NSObject, CBCentralManagerDelegate {
+    var centralManager: CBCentralManager!
+    
+    func start() {
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: [CBUUID(string: "4c052726-cd97-4dde-9356-212cc1327a84")])
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if let serviceData = advertisementData["kCBAdvDataServiceData"] as? NSDictionary {
+            for key in serviceData.allKeys {
+                if let data = serviceData[key] as? Data {
+                    if data.count >= 7 {
+                        NSLog("Detected not-a-beacon")
+                        NSLog("Service: \(key)");
+                        NSLog("Data values: \(data.hexDescription)")
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension Data {
+    var hexDescription: String {
+        return reduce("") {$0 + String(format: "%02x", $1)}
+    }
+}
---
+++ NotABeacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotABeacon
+//
+//  Created by David G. Young on 11/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
